88ac461ff55301b2def02824bc5385fdc21f7082
Phoenix Schneider <10908806@uvu.edu>
2021-09-23 20:52:12 -0600

    Updated name of Main player to NewBehaviourScript
    To try and fix the error I was getting in Unity when I tried attaching the script to a file.

--- Fall Term/Assets/NewBehaviourScript.cs
+++ Fall Term/Assets/NewBehaviourScript.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Use this for initialization 
-    void Start () {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
-    
+
     // Update is called once per frame
-    void Update () { 
+    void Update()
+    {
         
     }
-        
 }
