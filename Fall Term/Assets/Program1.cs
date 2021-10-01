04ee4e17b350b014725ee2d6132611958689fd87
Phoenix Schneider <10908806@uvu.edu>
2021-09-30 21:03:08 -0600

    trying to fix errors

--- Fall Term/Assets/Program1.cs
+++ Fall Term/Assets/Program1.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class Program1 : MonoBehaviour
+
+namespace MyApplication
 {
-    // Start is called before the first frame update
-    void Start()
+    class Program
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        static void Main(string[] args)
+        {
+            int x = 25;
+            int y = 20;
+            Console.WriteLine(x == y);  // returns False because 25 is not equal to 20
+        }
     }
 }
