9f7822ddc89c2f20db1a34a58f14ab5dbd77f3e2
Phoenix Schneider <10908806@uvu.edu>
2021-10-06 20:29:00 -0600

    Added Conditions and if statements

---
+++ Fall Term/Assets/Program2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 30;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+
