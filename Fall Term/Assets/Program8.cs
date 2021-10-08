e1006291cd628fbe3c34ae5958ae240f1993b4fc
Phoenix Schneider <10908806@uvu.edu>
2021-10-07 21:38:14 -0600

    Created an Array

---
+++ Fall Term/Assets/Program8.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Program8 : MonoBehaviour
+{
+    public Program8()
+    {
+    }
+}
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(cars[0]);
+        }
+    }
+}
