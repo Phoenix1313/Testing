1d21713ec750431c794b0e7f60abb2a3605417be
Phoenix Schneider <10908806@uvu.edu>
2021-10-07 21:30:35 -0600

    Added While Loop

---
+++ Fall Term/Assets/Program6.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
