8de22afa2ffd6299804125932d5cdc2a833c9975
Phoenix Schneider <10908806@uvu.edu>
2021-10-08 20:35:16 -0600

    Forreach Loop Added

--- Fall Term/Assets/Program8.cs
+++ Fall Term/Assets/Program8.cs
@@ -35,3 +35,14 @@ namespace MyApplication
         }
     }
 }
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+                    foreach (string i in cars)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
